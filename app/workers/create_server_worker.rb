1c73ab85bc601277f5175cdb3474803a30798c91
Dan Vera <arubinofaux@gmail.com>
2020-06-29 20:20:10 -0400

    double check for existing records

--- app/workers/create_server_worker.rb
+++ app/workers/create_server_worker.rb
@@ -5,21 +5,25 @@ class CreateServerWorker
 
   def perform(payload)
 
-    Server.create!(
-      players: payload["players"],
-      server_version: payload["server_version"],
-      region: payload["region"],
-      name: payload["name"],
-      address: payload["address"],
-      server_type: payload["server_type"],
-      last_time: payload["last_time"],
-      battle_type: payload["battle_type"],
-      start_time: payload["start_time"],
-      state: payload["state"],
-      players_max: payload["players_max"],
-      password: payload["password"],
-      camera_type: payload["camera_type"]
-    )
+    find_server = Server.find_by_address(payload["address"])
+
+    unless find_server
+      Server.create!(
+        players: payload["players"],
+        server_version: payload["server_version"],
+        region: payload["region"],
+        name: payload["name"],
+        address: payload["address"],
+        server_type: payload["server_type"],
+        last_time: payload["last_time"],
+        battle_type: payload["battle_type"],
+        start_time: payload["start_time"],
+        state: payload["state"],
+        players_max: payload["players_max"],
+        password: payload["password"],
+        camera_type: payload["camera_type"]
+      )
+    end
       
   end
 
